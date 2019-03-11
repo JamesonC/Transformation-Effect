6fdcb86d46273989c1c2feaa80bc3195eec675f9
Jameson Campbell <jcampbell@flexterapp.com>
2019-03-11 00:20:12 -0500

    linked button to repo

--- js/index.js
+++ js/index.js
@@ -1,17 +1,12 @@
 $(document).ready(function () {
   const $app = $('.app');
-  const $img = $('.app__img');
   const $pageNav1 = $('.pages__item--1');
-  const $pageNav2 = $('.pages__item--2');
   let animation = true;
-  let curSlide = 3;
-  let scrolledUp, nextSlide;
+  let curSlide = 5;
 
 // Controls speed of initial webpage load transformation //
 
   setTimeout(function () {
     $app.addClass('initial');
   }, 500);
-
-
 });
